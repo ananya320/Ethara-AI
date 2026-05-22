fc0e33b06fabdba66dde05596a8f6e51bc159fb9
ananya320 <rajvanshiananyasingh@gmail.com>
2026-05-22 12:25:20 +0530

    Fix package-lock.json sync

--- team-task-manager/frontend/ethara/src/api.js
+++ team-task-manager/frontend/ethara/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://ethara-backend-production-0e70.up.railway.app/" });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
